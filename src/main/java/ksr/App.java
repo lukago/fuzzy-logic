9a15b44724db6171aaec2de334a6f8663d39f316
jerrrrycho <203961@edu.p.lodz.pl>
2018-06-03 15:06:19 +0200

    Added test, removed code from App

--- src/main/java/ksr/App.java
+++ src/main/java/ksr/App.java
@@ -20,8 +20,9 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         List<Person> people = new CsvPersonDao("data/adult.test").read();
-        List<Quantyfier> quantyfiers = new JsonQuantifierDao("data/quantificators3.json", people.size()).read();
+        List<Quantyfier> quantyfiers = new JsonQuantifierDao("data/quantificators.json", people.size()).read();
         List<Double> weights1 = new JsonWeightsDao("data/weights1.json").read();
+        List<Double> weights3 = new JsonWeightsDao("data/weights3.json").read();
         List<FuzzySet> fuzzySets = new ArrayList<>();
 
         FuzzySet middleAge = new FuzzySet(
@@ -39,132 +40,17 @@ public class App {
                 new TrapezoidalFunction(56, 65, 100, 110),
                 people.stream().map(Person::getAge).collect(Collectors.toList()));
 
-        /*FuzzySet avarageSalaries = new FuzzySet(
-                "average salaries",
-                new TrapezoidalFunction(1000, 2000, 30000000, 40000000),
-                people.stream().map(s -> (double) s.getSalary()).collect(Collectors.toList()));
-*/
+
         fuzzySets.add(youngAge);
         fuzzySets.add(middleAge);
         fuzzySets.add(oldAge);
-        //fuzzySets.add(avarageSalaries);
 
-
-        //System.out.println(LinguisticAnswer.answerSets(fuzzySets, quantyfiers));
-
-
-        FuzzySet highSalary = new FuzzySet(
-                "high salary",
-                new TrapezoidalFunction(300000, 450000, 3000000, 5000000),
-                people.stream().map(s -> (double) s.getSalary()).collect(Collectors.toList()));
-
-        FuzzySet averageSalary = new FuzzySet(
-                "average salary",
-                new TrapezoidalFunction(80000, 170000, 250000, 300000),
-                people.stream().map(s -> (double) s.getSalary()).collect(Collectors.toList()));
-
-        FuzzySet lowSalary = new FuzzySet(
-                "low salary",
-                new TrapezoidalFunction(10000, 25000, 45000, 60000),
-                people.stream().map(s -> (double) s.getSalary()).collect(Collectors.toList()));
-
-
-        fuzzySets.add(highSalary);
-        fuzzySets.add(averageSalary);
-        fuzzySets.add(lowSalary);
-
-        FuzzySet highWeeklyLoad = new FuzzySet(
-                "high weekly load",
-                new TrapezoidalFunction(50, 55, 80, 100),
-                people.stream().map(s -> (double) s.getHoursPerWeek()).collect(Collectors.toList()));
-
-        FuzzySet averageWeeklyLoad = new FuzzySet(
-                "average weekly load",
-                new TrapezoidalFunction(30, 35, 45, 50),
-                people.stream().map(s -> (double) s.getHoursPerWeek()).collect(Collectors.toList()));
-
-        FuzzySet lowWeeklyLoad = new FuzzySet(
-                "low weekly load",
-                new TrapezoidalFunction(15, 18, 22, 30),
-                people.stream().map(s -> (double) s.getHoursPerWeek()).collect(Collectors.toList()));
-
-        fuzzySets.add(highWeeklyLoad);
-        fuzzySets.add(averageWeeklyLoad);
-        fuzzySets.add(lowWeeklyLoad);
-
-      /*  List<Qualifier> qualifiers = new ArrayList<>();
-        Qualifier raceQualifier = new Qualifier(
-                "black race",
-                new DiscreteFunction(Race.BLACK.ordinal()),
-                people.stream().map(s -> (double) s.getRace().ordinal()).collect(Collectors.toList()),
-                averageSalary
-        );
-        Qualifier avarageAgeTriangleAvarageSalaries = new Qualifier(
-                "middle age",
-                new TrapezoidalFunction(25, 35, 45, 50),
-                people.stream().map(Person::getAge).collect(Collectors.toList()),
-                averageSalary);
-        Qualifier managersAvarageSalaries = new Qualifier(
-                "managers",
-                new DiscreteFunction(Occupation.EXEC_MANAGERIAL.ordinal()),
-                people.stream().map(s -> (double) s.getOccupation().ordinal()).collect(Collectors.toList()),
-                averageSalary
-        );
-
-        qualifiers.add(avarageAgeTriangleAvarageSalaries);
-        qualifiers.add(raceQualifier);
-        qualifiers.add(managersAvarageSalaries);
-        System.out.println(LinguisticAnswer.answerQualifiers(qualifiers, quantyfiers));
-*/
-        FuzzySet bachelors = new FuzzySet(
-                "bachelors",
-                new DiscreteFunction(Education.BACHELORS.ordinal()),
-                people.stream().map(s -> (double) s.getEducation().ordinal()).collect(Collectors.toList()));
-
-        FuzzySet masters = new FuzzySet(
-                "masters",
-                new DiscreteFunction(Education.MASTERS.ordinal()),
-                people.stream().map(s -> (double) s.getEducation().ordinal()).collect(Collectors.toList()));
-
-        FuzzySet doctorates = new FuzzySet(
-                "doctorates",
-                new DiscreteFunction(Education.DOCTORATE.ordinal()),
-                people.stream().map(s -> (double) s.getEducation().ordinal()).collect(Collectors.toList()));
-
-        fuzzySets.add(bachelors);
-        fuzzySets.add(masters);
-        fuzzySets.add(doctorates);
-
-        FuzzySet blacks = new FuzzySet(
-                "black race",
-                new DiscreteFunction(Race.BLACK.ordinal()),
-                people.stream().map(s -> (double) s.getRace().ordinal()).collect(Collectors.toList()));
-
-        FuzzySet whites = new FuzzySet(
-                "white race",
-                new DiscreteFunction(Race.WHITE.ordinal()),
-                people.stream().map(s -> (double) s.getRace().ordinal()).collect(Collectors.toList()));
-
-        FuzzySet asians = new FuzzySet(
-                "asian race",
-                new DiscreteFunction(Race.ASIAN_PAC_ISLANDER.ordinal()),
-                people.stream().map(s -> (double) s.getRace().ordinal()).collect(Collectors.toList()));
-
-        FuzzySet others = new FuzzySet(
-                "other race",
-                new DiscreteFunction(Race.OTHER.ordinal()),
-                people.stream().map(s -> (double) s.getRace().ordinal()).collect(Collectors.toList()));
-
-        fuzzySets.add(blacks);
-        fuzzySets.add(whites);
-        fuzzySets.add(asians);
-        fuzzySets.add(others);
 
 
         System.out.println(oldAge.getValues().stream().mapToInt(Double::intValue).average());
-        System.out.println(highSalary.getValues().stream().mapToInt(Double::intValue).average());
-        System.out.println(highWeeklyLoad.getValues().stream().mapToInt(Double::intValue).average());
+
         System.out.println(LinguisticAnswer.answerSets(fuzzySets, quantyfiers, weights1));
+        System.out.println(LinguisticAnswer.answerSets(fuzzySets, quantyfiers, weights3));
 
 
     }
